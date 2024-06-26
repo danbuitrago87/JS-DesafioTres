1d5e07a70326abe4da608f142ae94fa23224ed39
unknown <danbuitrago87@gmail.com>
2024-06-26 13:48:08 -0500

    ThirdCommit While y contador

--- app1.js
+++ app1.js
@@ -1,15 +1,19 @@
 //DEFINIENDO EL CICLO WHILE PARA EL JUEGO DE NUMERO
 let numSecret=85;
 let num=0;
+let cont=0;
 
 while(num!=numSecret){
     let num=prompt("Digita un número del 1 al 100: ");
+    cont++;
     if (num==numSecret){
-        alert("Acertaste");
+        alert("Acertaste");  
+        alert(`El número de intentos fue: ${cont}`);
+        cont=0;
     }else{
         if (num<numSecret)
             alert("El número es mayor...");
         if (num>numSecret)
             alert("El número es menor...")
     }
-}
+} 
