f81446a4d54065d8d8b34259c8015fc7e94b89fe
unknown <danbuitrago87@gmail.com>
2024-06-27 11:47:40 -0500

    Mejora con Breack y numero de intentos

--- app1.js
+++ app1.js
@@ -2,7 +2,9 @@
 let numSecret=85;
 let num=0;
 let cont=0;
+let intentos=3;
 
+alert("BIENVENIDO AL JUEGO DEL NÚMERO SECRETO, TIENES 3 INTENTOS");
 while(num!=numSecret){
     let num=prompt("Digita un número del 1 al 100: ");
     cont++;
@@ -10,10 +12,17 @@ while(num!=numSecret){
         alert("Acertaste");  
         alert(`El número de intentos fue: ${cont}`);
         cont=0;
+        break;
     }else{
+        intentos--;
+        if (intentos==0){
+            alert("PERDISTE");
+            break;
+        }
         if (num<numSecret)
             alert("El número es mayor...");
         if (num>numSecret)
             alert("El número es menor...")
+        alert(`Te quedan ${intentos} intentos`)
     }
 } 
