c0f5d005cab67b0f1950a6a86428b7f29d2cae2b
unknown <danbuitrago87@gmail.com>
2024-06-27 16:20:18 -0500

    metodo Math.random y parseInt

--- app1.js
+++ app1.js
@@ -1,16 +1,18 @@
 //DEFINIENDO EL CICLO WHILE PARA EL JUEGO DE NUMERO
-let numSecret=85;
+//let numSecret=85;
 let num=0;
 let cont=0;
 let intentos=3;
 
 alert("BIENVENIDO AL JUEGO DEL NÚMERO SECRETO, TIENES 3 INTENTOS");
+let numSecret=parseInt(Math.random()*10);//la funcion random escoge un numero entre 0 y 1, parseInt lo convierte a entero
+console.log(numSecret);//se muestra en la consola
 while(num!=numSecret){
-    let num=prompt("Digita un número del 1 al 100: ");
+    let num=prompt("Digita un número del 1 al 10: ");
     cont++;
     if (num==numSecret){
         alert("Acertaste");  
-        alert(`El número de intentos fue: ${cont}`);
+        alert(`El número de intentos fue: ${cont} ${cont==1 ? 'vez':'veces'}`);//operador ternario
         cont=0;
         break;
     }else{
